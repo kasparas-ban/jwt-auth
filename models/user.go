65df713e74ba1e0af2f71936cf91d5ea98074172
Kasparas <kasparas.baniulis@gmail.com>
2022-09-02 16:38:01 +0300

    Cleaned up requests

--- models/user.go
+++ models/user.go
@@ -12,15 +12,6 @@ type User struct {
 	Password string `json:"password"`
 }
 
-func (user *User) HashPassword(password string) error {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	if err != nil {
-		return err
-	}
-	user.Password = string(bytes)
-	return nil
-}
-
 func (user *User) CheckPassword(providedPassword string) error {
 	err := bcrypt.CompareHashAndPassword(
 		[]byte(user.Password),
